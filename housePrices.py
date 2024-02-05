57dc5678d9925042d814d2d3e7b4eba439b0994e
Marcelino Bautista <70731793+alonelycharmander@users.noreply.github.com>
2024-02-04 16:19:13 -1000

    reading in the data

--- housePrices.py
+++ housePrices.py
@@ -1,1 +1,12 @@
-print("hello world")
+import pandas as pd 
+import tensorflow 
+dataset_df = pd.read_csv("train.csv")
+
+#printin the head 
+print(dataset_df.head(5))
+
+#prints the columns withe the count and data type 
+print(dataset_df.info())
+
+
+
